37bb44404f1c4d0fb6f5d0e2575d9f3aae672200
revathi1995 <60250845+revathi1995@users.noreply.github.com>
2020-10-30 11:56:52 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Router } from "react-router-dom";
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={createHistory({ basename: process.env.PUBLIC_URL })}>
+  <Router history={history}>
     <App />
   </Router>,
   document.getElementById('root')
@@ -15,5 +17,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
